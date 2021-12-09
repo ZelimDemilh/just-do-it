6d910861cff9783c699d33fcade385407ffcf45c
Зелимхан Демильханов <zelemhanzzz@mail.ru>
2021-12-09 09:53:45 +0300

    CRUD операции для модели Task

--- routes/task.router.js
+++ routes/task.router.js
@@ -1,5 +1,11 @@
 const {Router} = require("express")
+const { taskController } = require("../controllers/task.controller")
 
 const router = Router()
+
+router.post("/add", taskController.addTask)
+router.get("/", taskController.getAllTask)
+router.get("/:id", taskController.getTaskById)
+router.delete("/remove/:id", taskController.removeTask)
 
 module.exports = router

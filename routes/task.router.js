f07b0df0d7dcbfcdaf1fe8eac4a2c95b3b5de285
Satuev <alitm.s@mail.ru>
2021-12-12 01:03:21 +0300

    регистрация, авторизация и мидвайр

--- routes/task.router.js
+++ routes/task.router.js
@@ -1,12 +1,13 @@
-const {Router} = require("express")
+const { Router } = require("express")
 const { taskController } = require("../controllers/task.controller")
+const authMiddlewares = require("../middlewares/auth.middlewares")
 
 const router = Router()
 
-router.post("/add", taskController.addTask)
+router.post("/add", authMiddlewares, taskController.addTask)
 router.get("/", taskController.getAllTask)
 router.get("/:id", taskController.getTaskById)
-router.delete("/remove/:id", taskController.removeTask)
-router.patch("/update/:id", taskController.updateTask)
+router.delete("/remove/:id", authMiddlewares, taskController.removeTask)
+router.patch("/update/:id", authMiddlewares, taskController.updateTask)
 
 module.exports = router

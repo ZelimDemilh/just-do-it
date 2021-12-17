3be7987b14b9fe72bb971c942a5ec14a34d07f28
Atamazow <atamazov99@mail.ru>
2021-12-17 14:38:30 +0300

    Сделал роут или контроллер для юсера

--- routes/user.router.js
+++ routes/user.router.js
@@ -5,7 +5,7 @@ const router = Router()
 
 router.post("/registration", usersController.registration) // Добавление пользователя
 router.post("/login", usersController.login) // Отправление данных пользователя
-router.get("/profile/:id") // Получение данных пользователя
+router.get("/profile/:id",usersController.getUserById) // Получение данных пользователя
 router.delete("/remove/:id") // Удаление пользователя
 router.get("/", usersController.getAllUsers) // Удаление пользователя
 
